81ddc17a5cb16b8bd99b3d820493f8feec0668eb
codr-mayank <44097941+codr-mayank@users.noreply.github.com>
2022-06-15 11:17:47 +0530

    add footer

--- app.vue
+++ app.vue
@@ -25,7 +25,33 @@
         >
           Get Started
         </a>
+
+        <!-- Hamburger Icon -->
+        <!-- <button id="menu-btn" class="block hamburger md:hidden focus:outline-none">
+          <span class="hamburger-top"></span>
+          <span class="hamburger-middle"></span>
+          <span class="hamburger-bottom"></span>
+        </button> -->
       </div>
+
+      <!-- Mobile Menu -->
+      <!-- <div class="md:hidden">
+        <div
+          id="menu"
+          class="absolute hidden sm:flex flex-col items-center self-end py-8
+            mt-10 space-y-6 font-bold bg-white sm:w-auto sm:self-center
+            left-6 right-6 drop-shadow-md"
+        >
+          <a href="#">Pricing</a>
+          <a href="#">Product</a>
+          <a href="#">About Us</a>
+          <a href="#">Careers</a>
+          <a href="#">Community</a>
+        </div>
+      </div> -->
+
+      <!-- 1:11:00 -->
+
     </nav>
 
     <!-- Hero Section -->
@@ -249,5 +275,115 @@
         </div>
       </div>
     </section>
+
+    <!-- CTA Section -->
+    <section id="cta" class="bg-brightRed">
+      <!-- Flex Container -->
+      <div
+        class="container flex flex-col items-center justify-between
+          px-6 py-24 mx-auto space-y-12 md:py-12 md:flex-row md:space-y-0"
+      >
+        <!-- Heading -->
+        <h2
+          class="text-5xl font-bold leading-tight text-center
+          text-white md:text-4xl md:max-w-xl md:text-left"
+        >
+          Simplify how your team works today
+        </h2>
+        <!-- Button -->
+        <div>
+          <a
+            href="#"
+            class="p-3 px6 py-2 text-brightRed bg-white rounded-full
+              shadow-2xl baseline hover:bg-gray-300"
+          >
+            Get Started
+          </a>
+        </div>
+      </div>
+    </section>
+
+    <!-- Footer -->
+    <footer class="bg-veryDarkBlue">
+      <!-- Flex Container -->
+      <div
+        class="container flex flex-col-reverse justify-between px-6
+          py-10 mx-auto space-y-8 md:flex-row md:space-y-0"
+      >
+        <!-- Logo and social links container -->
+        <div
+          class="flex flex-col-reverse items-center justify-between
+            space-y-12 md:flex-col md:space-y-0 md:items-start"
+        >
+          <div class="mx-auto my-6 text-center text-white md:hidden">
+            Copyright &copy; 2022, All Rights Reserved
+          </div>
+          <!-- Logo -->
+          <div>
+            <img src="@/assets/img/logo-white.svg" class="h-8" alt="" />
+          </div>
+          <!-- Social Links Container -->
+          <div class="flex justify-center space-x-4">
+            <!-- Link 1 -->
+            <a href="#">
+              <img src="@/assets/img/icon-facebook.svg" alt="" class="h-8">
+            </a>
+            <!-- Link 2 -->
+            <a href="#">
+              <img src="@/assets/img/icon-youtube.svg" alt="" class="h-8">
+            </a>
+            <!-- Link 3 -->
+            <a href="#">
+              <img src="@/assets/img/icon-twitter.svg" alt="" class="h-8">
+            </a>
+            <!-- Link 4 -->
+            <a href="#">
+              <img src="@/assets/img/icon-pinterest.svg" alt="" class="h-8">
+            </a>
+            <!-- Link 5 -->
+            <a href="#">
+              <img src="@/assets/img/icon-instagram.svg" alt="" class="h-8">
+            </a>
+          </div>
+        </div>
+        <!-- List Container -->
+        <div class="flex justify-around space-x-32">
+          <div class="flex flex-col space-y-3 text-white">
+            <a href="#" class="hover:text-brightRed">Home</a>
+            <a href="#" class="hover:text-brightRed">Pricing</a>
+            <a href="#" class="hover:text-brightRed">Products</a>
+            <a href="#" class="hover:text-brightRed">About</a>
+          </div>
+          <div class="flex flex-col space-y-3 text-white">
+            <a href="#" class="hover:text-brightRed">Careers</a>
+            <a href="#" class="hover:text-brightRed">Community</a>
+            <a href="#" class="hover:text-brightRed">Privacy Policy</a>
+          </div>
+        </div>
+
+        <!-- Input Container -->
+        <div class="flex flex-col justify-between">
+          <form>
+            <div class="flex space-x-3">
+              <!-- input.flex-1 -->
+              <input
+                type="text"
+                class="flex-1 px-4 rounded-full focus:outline-none"
+                placeholder="Updated in your inbox"
+              />
+              <button
+                class="px-6 py-2 text-white rounded-full
+                bg-brightRed hover:bg-brightRedLight focus:outline-none"
+              >
+                Go
+              </button>
+            </div>
+          </form>
+          <div class="hidden text-white md:block">
+            Copyright &copy; 2022, All Rights Reserved
+          </div>
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
